efc822d9d97ad9093db7d337030c9c40b6b18e21
1-705-MAIN <akj.hoseo@gmail.com>
2020-09-26 11:10:19 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/itc/hoseo/cc/CarrotCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package itc.hoseo.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
